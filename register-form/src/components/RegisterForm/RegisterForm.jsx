fe7aedf3ff914a9bbf3ea123b34859438d81283f
Alberto Junior <albertojuniorhc@gmail.com>
2022-02-14 17:36:06 -0300

    Class02 - Atv2

--- register-form/src/components/RegisterForm/RegisterForm.jsx
+++ register-form/src/components/RegisterForm/RegisterForm.jsx
@@ -1,16 +1,33 @@
+import { Typography } from "@mui/material";
 import React from "react";
+import { useState } from "react";
 import DeliveryDetails from "../DeliveryDetails/DeliveryDetails";
 import PersonalDetails from "../PersonalDetails/PersonalDetails";
 import UserData from "../UserData/UserData";
 
 function RegisterForm({ onSubmitForm, isCPFValid }) {
-  return (
-    <>
-      <PersonalDetails onSubmitForm={onSubmitForm} isCPFValid={isCPFValid} />
-      <UserData />
-      <DeliveryDetails />
-    </>
-  );
+  const [currentStep, setCurrentStep] = useState(1);
+
+  const CurrentForm = (step) => {
+    switch (step) {
+      case 0:
+        return <UserData />;
+      case 1:
+        return (
+          <PersonalDetails
+            onSubmitForm={onSubmitForm}
+            isCPFValid={isCPFValid}
+          />
+        );
+
+      case 2:
+        return <DeliveryDetails />;
+      default:
+        return <Typography>Error on form select!</Typography>;
+    }
+  };
+
+  return <>{CurrentForm(currentStep)}</>;
 }
 
 export default RegisterForm;
