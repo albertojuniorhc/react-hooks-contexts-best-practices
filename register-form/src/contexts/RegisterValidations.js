8c51f48b9db2a9bdfd17ac4d5b8ce73ccc9a0642
Alberto Junior <albertojuniorhc@gmail.com>
2022-02-15 18:21:13 -0300

    Class05 - Atv2

--- register-form/src/contexts/RegisterValidations.js
+++ register-form/src/contexts/RegisterValidations.js
@@ -1,3 +1,12 @@
 import React from "react";
-const RegisterValidations = React.createContext();
+const RegisterValidations = React.createContext({
+  cpf: noValidation,
+  password: noValidation,
+  name: noValidation,
+});
+
+function noValidation(data) {
+  console.log(data);
+  return { isValid: true, text: "" };
+}
 export default RegisterValidations;
